06f02df815c5a490c9c1c7bbfee8dc3eb1886a7c
Zubair Abubakar <zuby52@gmail.com>
2025-08-30 10:29:13 +0100

    Explored k-means

--- machine-learning/unsupervised-learning/k-means.py
+++ machine-learning/unsupervised-learning/k-means.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import px
 from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
 from sklearn.preprocessing import StandardScaler
